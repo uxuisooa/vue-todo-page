05f46c7f0e3a31bc8b96d677495bd464315c4557
uiuxsooa <42193018+uiuxsooa@users.noreply.github.com>
2019-06-08 12:46:34 +0900

    프로젝트 세팅

---
+++ src/components/Header.vue
@@ -0,0 +1,47 @@
+<template>
+  <header class="header">
+    <h1>todos</h1>
+    <input
+      v-model="text"
+      class="new-todo"
+      autofocus
+      autocomplete="off"
+      placeholder="What needs to be done?"
+      @keypress="handleInput"
+    >
+    <!-- input과   return{text: "" }를-->
+    {{ text }}
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: ""
+    };
+  },
+  methods: {
+    handleInput (event) {
+      if (event.keyCode === 13) {
+        this.$emit("addTodo", this.text);
+      };
+    }
+  }
+};
+</script>
+
+<style>
+h1 {
+  position: absolute;
+  top: -155px;
+  width: 100%;
+  font-size: 100px;
+  font-weight: 100;
+  text-align: center;
+  color: rgba(175, 47, 47, 0.15);
+  -webkit-text-rendering: optimizeLegibility;
+  -moz-text-rendering: optimizeLegibility;
+  text-rendering: optimizeLegibility;
+}
+</style>
